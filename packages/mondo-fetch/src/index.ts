542d698f07e4f655f73124b9a44d79edd3f09ca6
mrus <Micah.Rus@ausnetservices.com.au>
2024-03-20 15:47:22 +1100

    Added more documentation and cleaned up some files

--- packages/mondo-fetch/src/index.ts
+++ packages/mondo-fetch/src/index.ts
@@ -1,18 +1,17 @@
 import {Result, raiseFailure, raiseSuccess} from '@mondopower/result-types'
-import {AbortError, BaseRequestOptions, ContentTypes, FetchClientConfig, FetchErrorTypes, HttpMethods, PostRequestOptions, PutRequestOptions} from './types'
+import {AbortError, ContentTypes, FetchClientConfig, FetchErrorTypes, RequestOptionsWithoutBody, HttpMethods, RequestOptions} from './types'
 export * from './types'
 
 export class FetchClient {
+  private readonly authorizationToken?: string | undefined
+  private readonly onBehalfOf?: string | undefined
+  private readonly baseUrl?: string | undefined
+
   /**
    * @param baseUrl An optional parameter that if provided will be prepended to each request
    * @param authorizationToken An optional token that will be provided to each request
    * @param onBehalfOf An optional user identifier that will be provided to each request
    */
-
-  private readonly authorizationToken?: string | undefined
-  private readonly onBehalfOf?: string | undefined
-  private readonly baseUrl?: string | undefined
-
   constructor(config?: FetchClientConfig) {
     this.baseUrl = config?.baseUrl
     this.authorizationToken = config?.authorizationToken
@@ -29,6 +28,9 @@ export class FetchClient {
     if (!baseUrl)
       return providedUrl
 
+    if (!providedUrl.startsWith('/') && !baseUrl.endsWith('/'))
+      return `${baseUrl}/${providedUrl}`
+
     return `${baseUrl}${providedUrl}`
   }
 
@@ -38,7 +40,7 @@ export class FetchClient {
    * @param requestOptions optional parameters that can be provided
    * @returns Promise<Result<T, FetchErrorTypes>>
    */
-  async get<T>(url: string, requestOptions?: BaseRequestOptions): Promise<Result<T, FetchErrorTypes>> {
+  async get<T>(url: string, requestOptions?: RequestOptionsWithoutBody): Promise<Result<T, FetchErrorTypes>> {
     const result = await this.createTimedFetchRequest<T>(this.getUrl(url, this.baseUrl), HttpMethods.GET, requestOptions)
     if (result.isErrored) {
       console.error(result.error.message)
@@ -54,7 +56,7 @@ export class FetchClient {
    * @param requestOptions optional parameters that can be provided
    * @returns Promise<Result<T, FetchErrorTypes>>
    */
-  public async post<T>(url: string, requestOptions?: PostRequestOptions): Promise<Result<T, FetchErrorTypes>> {
+  public async post<T>(url: string, requestOptions?: RequestOptions): Promise<Result<T, FetchErrorTypes>> {
     const result = await this.createTimedFetchRequest<T>(this.getUrl(url, this.baseUrl), HttpMethods.POST, requestOptions)
     if (result.isErrored) {
       console.error(result.error.message)
@@ -70,7 +72,7 @@ export class FetchClient {
    * @param requestOptions optional parameters that can be provided
    * @returns Promise<Result<T, FetchErrorTypes>>
    */
-  public async put<T>(url: string, requestOptions?: PutRequestOptions): Promise<Result<T, FetchErrorTypes>> {
+  public async put<T>(url: string, requestOptions?: RequestOptions): Promise<Result<T, FetchErrorTypes>> {
     const result = await this.createTimedFetchRequest<T>(this.getUrl(url, this.baseUrl), HttpMethods.PUT, requestOptions)
     if (result.isErrored) {
       console.error(result.error.message)
@@ -86,7 +88,7 @@ export class FetchClient {
    * @param requestOptions optional parameters that can be provided
    * @returns Promise<Result<T, FetchErrorTypes>>
    */
-  public async delete<T>(url: string, requestOptions?: BaseRequestOptions): Promise<Result<T, FetchErrorTypes>> {
+  public async delete<T>(url: string, requestOptions?: RequestOptionsWithoutBody): Promise<Result<T, FetchErrorTypes>> {
     const result = await this.createTimedFetchRequest<T>(this.getUrl(url, this.baseUrl), HttpMethods.DELETE, requestOptions)
     if (result.isErrored) {
       console.error(result.error.message)
@@ -108,6 +110,10 @@ export class FetchClient {
     return typedError.name === 'AbortError'
   }
 
+  private isRequestOptions(requestOptions: RequestOptions | RequestOptionsWithoutBody | undefined): requestOptions is RequestOptions {
+    return !!requestOptions && ('body' in requestOptions)
+  }
+
   /**
    * Creates a timed fetch request using an Abort Controller
    * @param url The url we are targeting for our request
@@ -115,28 +121,33 @@ export class FetchClient {
    * @param requestOptions optional parameters that can be provided
    * @returns Promise<Result<T, FetchErrorTypes>>
    */
-  private async createTimedFetchRequest<T>(url: string, method: HttpMethods, requestOptions?: PostRequestOptions)
+  private async createTimedFetchRequest<T>(url: string, method: HttpMethods, requestOptions?: RequestOptions | RequestOptionsWithoutBody)
     : Promise<Result<T, FetchErrorTypes>> {
 
-    const {body, contentType = ContentTypes.JSON, timeout = 30000} = requestOptions ?? {}
+    const {contentType = ContentTypes.JSON, timeout = 30000} = requestOptions ?? {}
+
 
     const headers = {
       'Content-Type': contentType,
     }
 
-    const authorizationToken = requestOptions?.authorizationToken || this.authorizationToken
+    const authorizationToken = requestOptions?.authorizationToken ?? this.authorizationToken
     if (authorizationToken)
       Object.assign(headers, {Authorization: `Bearer ${authorizationToken}`})
-    const onBehalfOf = requestOptions?.onBehalfOf || this.onBehalfOf
+
+    const onBehalfOf = requestOptions?.onBehalfOf ?? this.onBehalfOf
     if (onBehalfOf)
       Object.assign(headers, {'X-On-Behalf-Of': onBehalfOf})
 
     const controller = new AbortController()
     const timeoutId = setTimeout(() => controller.abort(), timeout)
 
+    const baseFetchRequest = {signal: controller.signal, method, headers}
+    const requestOptionsHasBody = this.isRequestOptions(requestOptions)
+
     try {
       controller.abort()
-      const result = await fetch(url, {signal: controller.signal, method, headers, body})
+      const result = await fetch(url, requestOptionsHasBody ? {...baseFetchRequest, body: requestOptions.body} : baseFetchRequest)
       clearTimeout(timeoutId)
 
       if (!result.ok)
--- packages/mondo-fetch/src/types.ts
+++ packages/mondo-fetch/src/types.ts
@@ -25,20 +25,23 @@ export interface FetchClientConfig {
   onBehalfOf?: string;
 }
 
-export interface PostRequestOptions extends BaseRequestOptions {
-  body?: string;
-  onBehalfOf?: string;
-}
 
-export interface PutRequestOptions extends BaseRequestOptions {
-  body?: string;
-  onBehalfOf?: string;
-}
+export type RequestOptionsWithoutBody = Omit<RequestOptions, 'body'>
 
-export interface BaseRequestOptions {
+/**
+   * RequestOptions that are optional parameters used for requests
+   * @param contentType The type of content @default 'application/json; charset=utf-8'
+   * @param timeout Optional parameter that dictates how long a request takes in milliseconds before timing out @default 30000
+   * @param authorizationToken An optional token that will be provided to each request @default undefined
+   * @param body Optional parameter that contains a payload to send with the request @default undefined
+   * @param onBehalfOf An optional user identifier that will be provided to each request @default undefined
+   */
+export interface RequestOptions {
   contentType?: ContentTypes;
   timeout?: number;
   authorizationToken?: string;
+  body?: string;
+  onBehalfOf?: string;
 }
 
 export interface AbortError {
